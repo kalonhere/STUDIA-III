43e22e21c89de7c72282d6d8b324dd2689392e2b
kalonhere <filipens1902@gmail.com>
2025-11-24 14:42:38 +0100

    chujnia

--- ASD/plan/plan.cpp
+++ ASD/plan/plan.cpp
@@ -32,30 +32,38 @@ int main(){
     }
     
     //cout << "nieposortowana lista: ";
-    for(int i =0; i<n;i++){
-        cout <<"["<<lista_zajec[i].start<<","<<lista_zajec[i].koniec<<") ";
-    }
+    //for(int i =0; i<n;i++){
+    //    cout <<"["<<lista_zajec[i].start<<","<<lista_zajec[i].koniec<<") ";
+    //}
     //    cout << "\n";
     qsort(lista_zajec,n,(sizeof(zajecie)),compare);
     
-    //cout << "posortowana lista:";
-    for(int i =0; i<n;i++){
-        cout <<"["<<lista_zajec[i].start<<","<<lista_zajec[i].koniec<<") ";
-    }
-    //cout << "\n";
+  // cout << "posortowana lista:";
+  // for(int i =0; i<n;i++){
+  //     cout <<"["<<lista_zajec[i].start<<","<<lista_zajec[i].koniec<<") ";
+  // }
+   //cout << "\n";
 
 
     int licznik_zajec = 0;
     zajecie ostatnie_zajecie;
     for(int i =0; i<n;i++){
-        if(i==0){licznik_zajec++; ostatnie_zajecie = lista_zajec[i];}
-        if((lista_zajec[i].start) == ostatnie_zajecie.koniec){
+        //cout <<"porownywany element: "<<lista_zajec[i].start << " " << lista_zajec[i].koniec << endl;
+        //cout<<"Ostatnie zajecie:"<<ostatnie_zajecie.start<<" "<<ostatnie_zajecie.koniec<<endl;
+        if(i==0){
+          licznik_zajec++;
+          ostatnie_zajecie = lista_zajec[i];
+          //cout<<"Przypisano pierwszy element:"<<ostatnie_zajecie.start<<" "<<ostatnie_zajecie.koniec<<endl;
+        }else if((lista_zajec[i].start) >= (ostatnie_zajecie.koniec)){
+            
             licznik_zajec++;
+            //cout << "Licznik zajec: "<<licznik_zajec;
             ostatnie_zajecie = lista_zajec[i];
-        }
+            //cout<<"ostatni element: "<<ostatnie_zajecie.start<<" "<<ostatnie_zajecie.koniec<<endl;
+        }else{continue;}
     }
     //cout << "licznik_zajec: "<<licznik_zajec;
-    cout << licznik_zajec;
+    cout << licznik_zajec << endl;
 
     
     delete[] lista_zajec;
