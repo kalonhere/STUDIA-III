7deea8fb8aee0d1271349ddb82b09a307a7b402c
kalonhere <filipens1902@gmail.com>
2025-11-30 14:13:43 +0100

    menu added

--- IOP/1zadanie_domowe/projekt1.cpp
+++ IOP/1zadanie_domowe/projekt1.cpp
@@ -1,11 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-  int n = 9;
-  int es = 1;
-  for(int i = 1; i <= n; i ++){
-    es = es * i;
+  int menu;
+  cout << "1==silnia" << endl;
+  cin >> menu;
+  if(menu == 1){
+    int n = 9;
+    int es = 1;
+    for(int i = 1; i <= n; i ++){
+      es = es * i;
+    }
+    cout << es << endl;
   }
-  cout << es << endl;
-
 }
