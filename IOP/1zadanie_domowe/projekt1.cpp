2570c217767553a7e69179b4df99f143f988d57d
kalonhere <filipens1902@gmail.com>
2025-11-30 14:21:02 +0100

    added menu for factorial

--- IOP/1zadanie_domowe/projekt1.cpp
+++ IOP/1zadanie_domowe/projekt1.cpp
@@ -2,13 +2,18 @@
 using namespace std;
 
 int main(){
-  int number = 29;
-  for(int i = 2; i*i <=number;i++){
-    if(number%i == 0){
-      cout << "number is not prime";
-      return 0;
+  cout << "2 == prime";
+  int menu;
+  cin >> menu;
+  if(menu == 2){
+    int number = 29;
+    for(int i = 2; i*i <=number;i++){
+      if(number%i == 0){
+        cout << "number is not prime";
+        return 0;
+      }
     }
+    cout << "number is prime";
   }
-  cout << "number is prime";
   return 0;
 }
