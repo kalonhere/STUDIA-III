cd6a034963875858741087bd686d22c56940dedb
kalonhere <filipens1902@gmail.com>
2026-01-11 23:58:58 +0100

    did 2nd excercise for siec-prog, and some PROG_2 file idk

---
+++ PROG_2/8home_wirtualne/essa.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+class Zwierze {
+public:
+  string nazwa;
+
+
+  string dajGlos(){
+    return "-- --";
+  }
+};
+
+class Kot : public Zwierze{
+  
+
+
+  Kot(string nazwa){
+
+  }
+
+}
+
+
+
+
+
+int main(){
+
+
+
+
+  return 0;
+}
--- SIEC-PROG/11zajeciaUDP/2zad.c
+++ SIEC-PROG/11zajeciaUDP/2zad.c
@@ -1,12 +1,25 @@
 #include<stdio.h>
-#include<strings.h>
+#include <stdlib.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
 #include<arpa/inet.h>
 
 
+int main(int argc, char*argv[]){
 
-int main(){
+  if(argc != 2){
+    printf("wrong argument count\n");
+    exit(EXIT_FAILURE);
+  }
+  struct in_addr network_address;
 
+  inet_pton(AF_INET,argv[1],&network_address);
 
+  printf("source: %s\n",argv[1]);
+  printf("sizeof: %lu || output: %d\n",sizeof(network_address.s_addr),network_address.s_addr);
+
+  printf("network->host long: %u\n",ntohl(network_address.s_addr));
+  printf("network->host short: %d\n",ntohs(network_address.s_addr));
 
   return 0;
 }
