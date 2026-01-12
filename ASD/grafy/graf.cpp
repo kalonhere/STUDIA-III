ca77f25bf1655579a1a2a5bc7a6d7182c71a2a03
kalonhere <filipens1902@gmail.com>
2026-01-12 11:17:05 +0100

    added asd excercise, and siec-prog udp dns excercise

--- ASD/grafy/graf.cpp
+++ ASD/grafy/graf.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
 
 int main(){
+    
 
 
-
+    
     return 0;
 }
---
+++ SIEC-PROG/12bsdudp/changedmysimplenslookup1.0.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<netdb.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+
+/* A. Mroz - zad. na SK, do modyfikacji */
+/* brak pelnej obslugi bledow! */
+
+int main(int argc, char * argv[]) {
+	
+	struct hostent *host;
+	char   **bufs;
+	struct in_addr *addr;
+
+	host = gethostbyname(argv[1]);
+	if(host == NULL) {
+		perror("Blad gethostbyname");
+		return -1;
+	}
+
+	printf("Pola struktury hostent:\n");
+	printf("h_name: %s\n", host->h_name);
+	printf("h_aliases: ");
+	for(bufs = host->h_aliases; *bufs != NULL; ++bufs)
+		printf("%s; ", *bufs);
+	
+	printf("\nh_addrtype: %d(%s)\n", host->h_addrtype, 
+			(host->h_addrtype == AF_INET)?"AF_INET":"nieznany typ adresu");
+	
+	printf("h_length: %d\n", host->h_length);
+	printf("h_addr_list: ");
+	for(bufs = host->h_addr_list; *bufs != NULL; ++bufs) {
+		addr = (struct in_addr *) *bufs;
+		printf("%s; ", inet_ntoa(*addr));
+	}
+
+	printf("\n");
+	return 0;
+
+}
+
+
