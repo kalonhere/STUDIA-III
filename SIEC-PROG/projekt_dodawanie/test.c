b84dd87172bf220b095a7df04ad3eab58a51d83d
kalonhere <filipens1902@gmail.com>
2026-01-13 22:40:24 +0100

    essa

--- SIEC-PROG/projekt_dodawanie/test.c
+++ SIEC-PROG/projekt_dodawanie/test.c
@@ -1,3 +1,4 @@
+#include <netinet/in.h>
 #include<stdio.h>
 #include <stdlib.h>
 #include<sys/socket.h>
@@ -13,16 +14,23 @@ void exitError(char * text){
 }
 
 int main(int argc, char *argv[]){
-  if(argc != 2){
-    exitError("wrong argument count");
-  }
+  struct sockaddr_in socket_address;
+  memset(&socket_address,0,sizeof(socket_address));
   
-  //checkAddressType(argv[1]);
+  socket_address.sin_addr.s_addr = INADDR_ANY;
+  socket_address.sin_port = htons((uint16_t)atoi(argv[2]));
+  socket_address.sin_family = AF_INET;
   
-  printf("string length: %lu",strlen(argv[1]));
-
-
-
+  int sock_fd = socket(AF_INET,SOCK_DGRAM,0);
+  if((bind(sock_fd,(struct sockaddr*)&socket_address,sizeof(socket_address))) == -1){
+    perror("bind");
+    exit(EXIT_FAILURE);
+  };
+  
+  char message[255] = {0};
+  recvfrom(sock_fd,message,255,0,NULL,NULL);
+  printf("received %s",message);
+  
 
   return 0;
 }
