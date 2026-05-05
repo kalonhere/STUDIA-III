5b76225e39fb3101a1a6d084dad4717169e42547
kalonhere <filipens1902@gmail.com>
2026-05-05 16:15:30 +0200

    added files with otoczka wypukla blyat

--- 2_ASD/otoczka/main.cpp
+++ 2_ASD/otoczka/main.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<vector>
 #include<fstream>
+#include<utility>
+#include<algorithm>
 using namespace std;
 
 typedef struct Node{
@@ -10,9 +12,18 @@ typedef struct Node{
 } Node;
 
 struct Tree{
+
+  //vektor nodeow
   vector<Node> node_list;
+  Node lowest_point;
 
 
+  //print nodes in terminal
+  void print_nodes(){
+    for(int i = 0; i < node_list.size(); i++){
+      cout << "ID: " << node_list[i].id << ", OX: " << node_list[i].OX << ", OY: " << node_list[i].OY << endl;
+    }
+  }
 
   int add_node(){
     //open file
@@ -31,8 +42,8 @@ struct Tree{
     int id;
     double ox;
     double oy;
-    Node input_node;
-    while(file >> id >> ox >> oy){
+    while(file>>id>>ox>>oy){
+      Node input_node;
       input_node.id = id;
       input_node.OX = ox;
       input_node.OY = oy;
@@ -41,7 +52,35 @@ struct Tree{
     }
     return 1;
   }
-};
+
+  Node findStartingPoint(){
+    lowest_point.OX = node_list[0].OX;
+    lowest_point.OY = node_list[0].OY;
+    for (int i = 1; i < node_list.size(); i++){
+      if(node_list[i].OY < lowest_point.OY){ // y mniejsze
+        lowest_point = node_list[i];
+      }else if ((node_list[i].OY == lowest_point.OY) && (node_list[i].OX < lowest_point.OX)){
+        lowest_point = node_list[i];
+      }
+    }//for
+    
+    cout << "Possible lowest point X: " << lowest_point.OX << " Y: " << lowest_point.OY << " ID: " << lowest_point.id << endl;
+    swap(node_list[0],node_list[(lowest_point.id-1)]);
+    return lowest_point;
+  }//find starting point
+
+
+  //void sortNodesAngle(){
+  //  vector<Node> sorted_vectors = node_list;
+  //  Node lowest_point_copy = lowest_point;    
+  //  sort(node_list.begin(),node_list.end(),[lowest_point_copy](Node pi, Node pj){
+
+  //      
+  //      });
+
+  //}
+  
+};//tree
 
 
 
@@ -51,7 +90,10 @@ int main(){
   if(tree.add_node()){
     cout << "Dodano node'y\n";
   }
+  cout << "ilosc nodeo'w: " << tree.node_list.size() << endl;
 
-
+  tree.print_nodes();
+  tree.findStartingPoint(); 
+  tree.print_nodes();
   return 0;
 }
